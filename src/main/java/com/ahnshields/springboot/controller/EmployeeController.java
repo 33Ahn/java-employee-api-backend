bd554c96cef8aeadc677d74e8180fc332994b3f0
Ahn Shields <ahnshields@gmail.com>
2022-10-07 18:48:16 -0700

    Build Update employee REST API

--- src/main/java/com/ahnshields/springboot/controller/EmployeeController.java
+++ src/main/java/com/ahnshields/springboot/controller/EmployeeController.java
@@ -32,7 +32,22 @@ public class EmployeeController {
     @GetMapping("{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
         Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id:" + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
         return ResponseEntity.ok(employee);
+    }
+
+    // build update employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails) {
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
     }
 }
